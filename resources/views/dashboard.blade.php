ac6c39aa5222014ae0b3db717f729536965bd710
Ilhammeru <gumilang.dev@gmail.com>
2022-06-14 03:02:15 +0700

    add automatic geo location

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,10 +12,45 @@
             </div>
         @endif --}}
 
-        {{ __('You are Admin') }}!
-        Dashboard Admin
+        <div class="row">
+            <div class="col">
+                <div class="button-section">
+                    <button class="btn btn-primary" id="btnGeo" type="button" onclick="getLocation()">Dapatkan Lokasi</button>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="target-geo">
+                    <p class="lat"></p>
+                    <p class="long"></p>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 <!--end::Card-->
 
 @endsection
+
+{{-- begin::scripts --}}
+@push('scripts')
+    <script>
+
+        function getLocation() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(showPosition);
+            } else { 
+                x.innerHTML = "Geolocation is not supported by this browser.";
+            }
+        }
+
+        function showPosition(position) {
+            let lat = position.coords.latitude;
+            let long = position.coords.longitude;
+            $('.lat').text(lat);
+            $('.long').text(long);
+        }
+    </script>
+@endpush
+{{-- end::scripts --}}
