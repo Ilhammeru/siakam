6345760f645b14f1bef3f02631ae83ea3c2c136c
Ilhammeru <gumilang.dev@gmail.com>
2022-06-29 07:50:48 +0700

    change

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -8,6 +8,16 @@
             <a target="_blank" class="text-gray-800 text-hover-primary">SiAKAM</a>
         </div>
         <!--end::Copyright-->
+        <!--begin::Menu-->
+        <ul class="menu menu-gray-600 menu-hover-primary fw-bold order-1">
+            <li class="menu-item">
+                <a target="_blank" class="menu-link px-2">About</a>
+            </li>
+            <li class="menu-item">
+                <a target="_blank" class="menu-link px-2">Support</a>
+            </li>
+        </ul>
+        <!--end::Menu-->
     </div>
     <!--end::Container-->
 </div>
